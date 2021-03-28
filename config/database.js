193d6b4695a59dac0ce39a759caa0696bfd32e71
edengebrezgi <edengebrezgi@gmail.com>
2021-03-28 19:29:44 -0400

    finished savage auth

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://edengebrezgi:aaliyah@cluster0.cjf2l.mongodb.net/savAuth?retryWrites=true&w=majority', 
+    'dbName': 'savAuth'
+};
